bfddc64f9bd2883e7d3711bab3e4089e88dbea3e
Joseph Quispe <josepheqch@gmail.com>
2020-09-29 23:09:05 -0500

    connection to db and routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 var bodyParser = require('body-parser');
 
@@ -8,33 +10,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-});
- 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-                body
-        });
-    }
-});
 
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+app.use(require('./routes/usuario'));
+
+mongoose.connect('mongodb://localhost:27017/cafe', //uri
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, //options
+    (err, res) => { //callback
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
     });
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto ', process.env.PORT);
 });
+
