e8a54eaef79dc8b6a1715d0871dfc46f64e0aa8d
Joseph Quispe <josepheqch@gmail.com>
2021-01-04 23:58:09 -0500

    continuing with the methods: put, get and deleted

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const app = express();
 
 app.get('/user', function (req, res) {
-    res.json('get user');
+
+    let from = req.query.from || 0;
+    from = Number(from);
+    let limit = req.query.limit || 5;
+    limit = Number(limit);
+
+    User.find({ status: true }, 'name email role img status google')
+        .skip(from)
+        .limit(limit)
+        .exec((err, users) => {
+            if( err ) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            User.countDocuments({ status: true }, (err, count) => {
+                res.json({
+                    ok: true,
+                    count,
+                    users
+                });
+            });
+
+        });
 });
 
 app.post('/user', function (req, res) {
@@ -28,15 +54,68 @@ app.post('/user', function (req, res) {
         res.json({
             ok: true,
             user: userDB
-        })
-    })
+        });
+    });
 });
 
 app.put('/user/:id', function (req, res) {
     let id = req.params.id;
-    res.json({
-        id
+    let body = _.pick(req.body, ['name','img','email','role','status'])
+
+    User.findByIdAndUpdate( id, body, {new: true, runValidators: true},(err, userDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDB
+        });
     });
+});
+
+app.delete('/user/:id', function (req, res) {
+    let id = req.params.id;
+    let status = { status: false }
+
+    User.findByIdAndUpdate( id, status, {new: true},(err, userDeleted) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDeleted
+        });
+    });
+
+    // User.findByIdAndRemove( id, (err, userDeleted) => {
+    //     if (err) {
+    //         return res.status(400).json({
+    //             ok: false,
+    //             err
+    //         });
+    //     }
+
+    //     if (!userDeleted) {
+    //         return res.status(400).json({
+    //             ok: false,
+    //             err: 'User not found'
+    //         });
+    //     }
+
+    //     res.json({
+    //         ok: true,
+    //         user: userDeleted
+    //     })
+    // });
+
 });
 
 module.exports = app;
