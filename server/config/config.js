7e2887df80b9b17c19ecf71505f90027d9969046
Joseph Quispe <josepheqch@gmail.com>
2021-01-14 07:54:09 -0500

    environment variable MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://devook:5yhgtfbYZjt36np8@cluster0.vgt31.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
