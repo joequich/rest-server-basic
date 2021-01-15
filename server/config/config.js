3c8879a6a44a656370f5baab81169d5cdd46f061
Joseph Quispe <josepheqch@gmail.com>
2021-01-15 08:10:35 -0500

    login

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,22 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 /**
+ * Expiration of token
+ * 60 seconds
+ * 60 minutes
+ * 24 hours
+ * 30 days
+ */
+
+process.env.EXPIRATION_TOKEN = 60 * 60 * 24 * 30;
+
+ /**
+ * Seed
+ */
+
+process.env.SEED = process.env.SEED || 'this-is-the-seed-of-development';
+
+/**
  * Data Base
  */
 
