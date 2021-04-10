105d123fe1eaa2f4f968b24048d3c2d354c5c51b
Joseph Quispe <josepheqch@gmail.com>
2021-04-09 21:31:27 -0500

    update login

---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const { request, response } = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+const login = async(req = request, res = response) => {
+    const { email, password } = req.body;
+
+    try {
+
+        //verify if email exist
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'User / Password arent\' corrects - email'
+            });
+        }
+
+        //if user is active
+        if (!user.status) {
+            return res.status(400).json({
+                msg: 'User / Password arent\' corrects - status: false'
+            });
+        }
+
+        //verify password
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'User / Password arent\' corrects - password'
+            });
+        }
+
+        //generate JWT
+        
+        res.json({
+            msg: 'Login ok'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Contact the administrator'
+        });
+    }
+}
+
+module.exports = {
+    login
+}
--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.userPath = '/api/users';
+        this.authPath = '/api/auth';
 
         // Connect to data base
         this.connectDB();
@@ -32,6 +33,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.userPath, require('../routes/users'));
     }
 
